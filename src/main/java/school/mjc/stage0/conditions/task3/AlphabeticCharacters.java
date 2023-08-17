4c53309a026fb12200b478ddf9b4f546b2405d6c
Martín Alcalá <carlos_a_v_bm@hotmail.com>
2023-08-17 01:30:55 -0600

    Conditions implemented

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,37 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character == 65 || character == 97){
+            System.out.println("Vowel");
+        }
+        if(character == 69 || character == 101){
+            System.out.println("Vowel");
+        }
+        if(character == 73 || character == 105){
+            System.out.println("Vowel");
+        }
+        if(character == 79 || character == 111){
+            System.out.println("Vowel");
+        }
+        if(character == 85 || character == 117){
+            System.out.println("Vowel");
+        }
+        if(character > 65 && character <= 68 || character > 97 && character <= 100){
+            System.out.println("Consonant");
+        }
+        if(character > 69 && character <= 72 || character > 101 && character <= 104){
+            System.out.println("Consonant");
+        }
+        if(character > 73 && character <= 78 || character > 105 && character <= 110){
+            System.out.println("Consonant");
+        }
+        if(character > 79 && character <= 84 || character > 111 && character <= 116){
+            System.out.println("Consonant");
+        }
+        if(character > 85 && character <= 90 || character > 117 && character <= 122){
+            System.out.println("Consonant");
+        }else{
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        int perCent;
+        if(salary <= 10000){
+            perCent = ((salary/2)/10)*3;
+            System.out.println(salary-perCent);
+        }
+        if(salary > 10000 && salary <= 20000){
+            perCent = (salary/100)*18;
+            System.out.println(salary-perCent);
+        }
+        if(salary > 20000){
+            perCent = (salary/10)*2;
+            System.out.println(salary-perCent);
+        }
+        if(salary == 0){
+            System.out.println("wrong input!");
+        }
     }
 }
