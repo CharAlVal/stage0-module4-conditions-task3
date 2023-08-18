3a9323ccf228ead3c3f2e16312a243960a2f6df1
Martín Alcalá <carlos_a_v_bm@hotmail.com>
2023-08-17 19:59:24 -0600

    Reverse ints in Aliquot formula

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
-        float result = first % second;
-        if(result == 0.0f){
+        int result = second % first;
+        if(result == 0){
             System.out.println("Aliquot");
         }else{
             System.out.println("Not aliquot");
